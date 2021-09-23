85862a396ebc5780cb6f40f517e20f07a4f37bec
johannes-titz <johannes@titz.science>
2021-09-23 13:39:43 +0200

    use sections in DATASET.R

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,4 +1,6 @@
 ## code to prepare `DATASET` dataset goes here
+
+# ice cream
 download.file("http://users.telenet.be/samuelfranssens/tutorial_data/icecream.xlsx",
               "iscreamforicecream.xlsx")
 icecream <- readxl::read_xlsx("iscreamforicecream.xlsx")
@@ -8,13 +10,14 @@ icecream <- as.data.frame(pivot_longer(icecream, cols = 6:20, names_to = "indivi
 write.csv(icecream, "data-raw/icecream.csv")
 usethis::use_data(icecream, overwrite = TRUE)
 
+# hdp
 hdp <- read.csv("https://stats.idre.ucla.edu/stat/data/hdp.csv")
 hdp <- hdp[, c("remission", "CancerStage", "Experience", "DID")]
 write.csv(hdp, "data-raw/hdp.csv")
 usethis::use_data(hdp, overwrite = TRUE)
 
+# school
 school <- read.table("http://bayes.acs.unt.edu:8083/BayesContent/class/Jon/R_SC/Module9/lmm.data.txt",
                    header=TRUE, sep=",", na.strings="NA", dec=".", strip.white=TRUE)
-
 write.csv(school, "data-raw/school.csv")
 usethis::use_data(school, overwrite = TRUE)
